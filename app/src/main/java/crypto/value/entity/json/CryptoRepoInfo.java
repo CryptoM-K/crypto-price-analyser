666a42aef3c58a8846911e84e243907d3bdb2985
minio96 <mi.sypek96@gmail.com>
2022-02-03 17:23:13 +0100

    App should use configuration file to manage cryptos # add configuration file and handle it # add service parsing config file

---
+++ app/src/main/java/crypto/value/entity/json/CryptoRepoInfo.java
@@ -0,0 +1,44 @@
+package crypto.value.entity.json;
+
+public class CryptoRepoInfo {
+    public String name;
+    public String repo;
+    public String path;
+    public String branch;
+
+    public CryptoRepoInfo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+
+    public void setRepo(String repo) {
+        this.repo = repo;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+}
